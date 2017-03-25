10b0f5a63933b86829db36354a65bac3a6b26c11
Xuchao <Xuchao@DESKTOP-DARFBMF>
2017-03-25 16:34:37 +1100

    Add project files.

---
+++ SUCSA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SUCSA.Startup))]
+namespace SUCSA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
