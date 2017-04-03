290b0cf8e0e4fd3e47c4dc1f700218e6658243ed
goodluck-yh <goodluck.yh2014@gmail.com>
2017-04-03 14:14:03 +1000

    add another layout for admin

--- SUCSA.DATA/Program.cs
+++ SUCSA.DATA/Program.cs
@@ -10,7 +10,7 @@ namespace SUCSA.DATA
     {
         static void Main(string[] args)
         {
-            using(var ctx=new SUCSAContext())
+            using (var ctx=new SUCSAContext())
             {
                 var admins = new List<Admin>
                 {
@@ -19,11 +19,13 @@ namespace SUCSA.DATA
                 };
                 var activities = new List<Activity>
                 {
-                    new Activity(){Category="BasketBall",IsTop=true,PictureName="1",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"F:\C#\SUCSA\SUCSA\img\Activities\1.jpg")) },
-                    new Activity(){Category="BasketBall",IsTop=true,PictureName="2",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"F:\C#\SUCSA\SUCSA\img\Activities\2.jpg")) },
-                    new Activity(){Category="BasketBall",IsTop=true,PictureName="3",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"F:\C#\SUCSA\SUCSA\img\Activities\3.jpg")) },
-                    new Activity(){Category="BasketBall",IsTop=true,PictureName="4",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"F:\C#\SUCSA\SUCSA\img\Activities\4.jpg")) },
-                    new Activity(){Category="BasketBall",IsTop=true,PictureName="5",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"F:\C#\SUCSA\SUCSA\img\Activities\5.jpg")) },
+
+                    
+                    new Activity(){Category="BasketBall",IsTop=true,PictureName="1",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"C:\Users\Administrator\Source\Repos\SUCSA\SUCSA\img\Activities\1.jpg")) },
+                    new Activity(){Category="BasketBall",IsTop=true,PictureName="2",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"C:\Users\Administrator\Source\Repos\SUCSA\SUCSA\img\Activities\2.jpg")) },
+                    new Activity(){Category="BasketBall",IsTop=true,PictureName="3",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"C:\Users\Administrator\Source\Repos\SUCSA\SUCSA\img\Activities\3.jpg")) },
+                    new Activity(){Category="BasketBall",IsTop=true,PictureName="4",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"C:\Users\Administrator\Source\Repos\SUCSA\SUCSA\img\Activities\4.jpg")) },
+                    new Activity(){Category="BasketBall",IsTop=true,PictureName="5",Picture=ByteHelper.ImageToByteArray(ByteHelper.LoadImageFromDisk(@"C:\Users\Administrator\Source\Repos\SUCSA\SUCSA\img\Activities\5.jpg")) },
                 };
                 foreach(Activity a in activities)
                 {
